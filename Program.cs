3b0943f7fe8740f1c632b1b5b706769d4f37c94d
Sobtrax <67792416+Sobtrax@users.noreply.github.com>
2023-01-09 13:24:47 +0100

    Naprawa paru błędów

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿while (true)
+﻿Console.WriteLine("Witaj w zgadnij liczbe, liczba jest mniejsza niż tysiąc i większa od zera");
+while (true)
 {
-    Console.WriteLine($"Witaj w zgadnij liczbe, liczba jest mniejsza niż tysiąc i większa od zera. \r\n Wpisz liczbe prób: ");
+    Console.WriteLine($"Wpisz liczbe prób: ");
     Random random = new Random();
     int num = random.Next(1000);
     string probInput = Console.ReadLine();
@@ -9,48 +10,56 @@
         Console.WriteLine("Podaj poprawną liczbę: ");
     }
 
-    for (int i = 0; i < liczbaProb; i++)
+    for (int i = 0; i < liczbaProb+1; i++)
     {
+        if (i == liczbaProb)
         {
-            Console.WriteLine("Zgadnij liczbe (do 1000): ");
-            string input = Console.ReadLine();
+            Console.WriteLine("Koniec prób. Zagrac jeszcze raz? (T/N)");
+            zagrac();
+            break;
+        }
+        Console.WriteLine("Zgadnij liczbe (do 1000): ");
+        string input = Console.ReadLine();
 
-            if (!int.TryParse(input, out int inputNum))
-            {
-                Console.WriteLine("To nie liczba!");
-            }
+        if (!int.TryParse(input, out int inputNum))
+        {
+            Console.WriteLine("To nie liczba!");
+        }
 
-            if (inputNum < num)
-            {
-                Console.WriteLine("Za mało!");
-            }
-            if (inputNum > num)
-            {
-                Console.WriteLine("Za dużo!");
-            }
-            if (inputNum == num)
-            {
-                Console.WriteLine("Zgadłeś! Zagrać jeszcze raz? (T/N)");
-                while (true)
-                {
-                    string zagrac = Console.ReadLine();
-                    zagrac = zagrac.ToUpper();
-                    if (zagrac != "T" && zagrac != "N")
-                    {
-                        Console.WriteLine("Niepoprawny znak. Zagrać jeszcze raz? (T/N)");
-                    }
-                    if (zagrac == "N")
-                    {
-                        Environment.Exit(0);
-                    }
-                    if (zagrac == "T")
-                    {
-                        break;
-                    }
-                }
-                break;
-            }
+        if (inputNum < num)
+        {
+            Console.WriteLine("Za mało!");
+        }
+        if (inputNum > num)
+        {
+            Console.WriteLine("Za dużo!");
+        }
+        if (inputNum == num)
+        {
+            Console.WriteLine("Zgadłeś! Zagrać jeszcze raz? (T/N)");
+            zagrac();
+            break;
+        }
+
+    }
+}
+static void zagrac()
+{
+    while (true)
+    {
+        string zagrac = Console.ReadLine();
+        zagrac = zagrac.ToUpper();
+        if (zagrac != "T" && zagrac != "N")
+        {
+            Console.WriteLine("Niepoprawny znak. Zagrać jeszcze raz? (T/N)");
+        }
+        if (zagrac == "N")
+        {
+            Environment.Exit(0);
+        }
+        if (zagrac == "T")
+        {
+            break;
         }
     }
 }
-
